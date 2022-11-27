fe0bc42fce45473a6283680010e37fdae7a426dd
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2022-11-27 11:32:49 -0300

    update OO

--- exercicios/horadecodar - section10/16_exercicios/script07.js
+++
@@ -1,5 +0,0 @@
-let frutas = ["Banana", "Mamão", "Maça", "Abacaxi", "Acabate", "Laranja"];
-
-for(let i = 0; i < frutas.length; i++) {
-    console.log(frutas[i])
-}
---
+++ exercicios/horadecodar - section12/01_metodos/script.js
@@ -0,0 +1,17 @@
+const cachorro = {
+    latir: function() {
+        console.log("Au au au");
+    },
+    uivar: function() {
+        console.log("Auuuuuuuuuuuuuuuuu...");
+    },
+    rosnar: function() {
+        console.log("Grrrr grrrrrrrr...");
+    }
+}
+
+// imprimindo os métodos abaixo:
+
+cachorro.latir();
+cachorro.uivar();
+cachorro.rosnar();
